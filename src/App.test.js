d8dae4a5783f747d2b24582e9f5d738afab10af8
darthzeran <ryan.mort@studentbody.byu.edu>
2023-06-23 17:43:13 -0700

    Add files via upload

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const iWroteTestsToTestMyCode = false;
+  const iPromiseIDoLikeToWriteTests = true;
+  expect(iWroteTestsToTestMyCode).toBe(false);
+  expect(iPromiseIDoLikeToWriteTests).toBe(true);
 });
